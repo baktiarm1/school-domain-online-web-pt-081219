f2c3daa96ed63512bfe31ce6b817ee305fc34b96
Baktiar Miah <baktiar.m@gmail.com>
2019-09-22 16:12:17 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School 
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def roster
+    @roster
+  end 
+  
+  def add_student(name, grade)
+    School.roster[grade] = []
+    School.roster[grade] << name
+  end 
+  
+  def grade(grade)
+    School.roster[grade]
+  end 
+  
+end 
